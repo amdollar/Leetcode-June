a06cf92474b3e39ff1322a754d06d0b9222cf4d2
Anurag awasthi <33833603+amdollar@users.noreply.github.com>
2020-06-02 00:19:55 +0530

    Invert a binary tree.
    Desc: Invert a binary tree.

---
+++ TreeNode.java
@@ -0,0 +1,24 @@
+package com.leetcode.week1;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(int val) {
+		super();
+		this.val = val;
+	}
+
+	public TreeNode() {
+		super();
+	}
+
+	public TreeNode(int val, TreeNode left, TreeNode right) {
+		super();
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+
+}
